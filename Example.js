8b05ddd02398a5e23766ac2a334db95901d6d64e
ChandanaRHegde <chandubengle2@gmail.com>
2024-12-11 18:02:39 +0530

    New js Code

---
+++ Example.js
@@ -0,0 +1,7 @@
+// Select the button using its ID
+const button = document.getElementById('clickMeButton');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+    alert('Hello! You clicked the button.');
+});
